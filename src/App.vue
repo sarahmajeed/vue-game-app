121fb5cd440f36ccc2c1168a3192fd800d53b3be
sarahmajeed <sarahmajeed368@gmail.com>
2021-06-04 12:44:35 -0700

    designing first page starter

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,89 @@
 <template>
   <div id="app" class="contain">
-    <h1>Hi there</h1>
+    <GameStateStart v-if="uiState === 'start'">
+      <!-- SLOTS -->
+      <h2>Which hooman do you wannabe?</h2>
+      <p v-for="character in characters" :key="character" class="character-choices"> 
+       <input v-model="characterinput" :id="character" :value="character" type="radio">
+        <label :for="character">{{character}}</label>
+      </p>
+    </GameStateStart>
+    <section v-else>
+    <svg viewBox="0 -180 1628 1180" class="main">
+  <defs>
+    <clipPath id="bottom-clip">
+      <rect class="bottom-clip-path" x="1131.5" y="546.5" width="406" height="1000" />
+    </clipPath>
+    <clipPath id="top-clip">
+      <rect class="top-clip-path" x="1131.5" y="69.5" width="406" height="473" />
+    </clipPath>
+  </defs>
+
+  <text
+    x="1000"
+    y="930"
+    style="font: normal 45px 'Recursive; text-transform: uppercase;"
+    class="text"
+  >Character Name</text>
+
+  <path fill="#f0959f" d="M0 842h657v192H0z" />
+
+  <g id="friendbubble">
+    <path
+      class="cls-45"
+      d="M342.3 471.8h106.6c0 42.6-21.3 63.9-21.3 63.9 64 0 85.3-63.9 85.3-63.9h42.6c42.7 0 42.7-42.7 42.7-42.7s21.3-149.3 21.3-191.9-42.6-42.7-42.6-42.7H321c-42.7 0-42.7 42.7-42.7 42.7l21.4 191.9s0 42.7 42.6 42.7z"
+      transform="translate(17)"
+    />
+    <path
+      class="cls-20"
+      d="M333.8 463.2h106.6c0 42.7-21.3 64-21.3 64 63.9 0 85.3-64 85.3-64H547c42.7 0 42.7-42.6 42.7-42.6s21.3-149.3 21.3-192-42.7-42.6-42.7-42.6H312.4c-42.6 0-42.6 42.6-42.6 42.6l21.3 192s0 42.6 42.7 42.6z"
+      transform="translate(17)"
+    />
+  </g>
+
+  <g id="alienbubble">
+    <path
+      class="cls-45"
+      d="M948.3 344.5c-103.8 0-187.9 76.3-187.9 170.4s84.1 170.3 187.9 170.3a201.5 201.5 0 00100.5-26.4l87.4 26.4-29.1-79.2c18.4-26.4 29.1-57.6 29.1-91.1 0-94.1-84.1-170.4-187.9-170.4z"
+      transform="translate(17)"
+    />
+    <path
+      class="cls-20"
+      d="M938.9 336C835.1 336 751 412.3 751 506.4s84.1 170.3 187.9 170.3a201.5 201.5 0 00100.5-26.4l87.4 26.4-29.1-79.2c18.4-26.4 29.1-57.6 29.1-91.1 0-94.1-84.1-170.4-187.9-170.4z"
+      transform="translate(17)"
+    />
+  </g>
+    </svg>
+    </section>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ import {mapState} from 'vuex'
+
+import GameStateStart from './components/GameStateStart.vue';
 
 export default {
   name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
   components: {
-    HelloWorld
+    GameStateStart
+    
+  },
+  data() {
+    return {
+      characterinput: ''
+    }
+  },
+   computed: {
+    ...mapState ([
+      'uiState',
+      'questions',
+      'characters'
+    ])
   }
 }
 </script>
@@ -19,7 +92,7 @@ export default {
 body {
   margin: 0;
   padding: 0;
-  font-family: "Recursive", Helvetica, Arial, sans-serif;
+  font-family: 'Recursive', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   font-size: 19px;
